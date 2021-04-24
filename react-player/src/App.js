e5f17c74ebaceec6407b15d214ec4df043f0e70b
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-24 11:18:56 +0100

    added components in App.js

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
+// Adding Components
+import Song from "./components/Song";
+import Player from "./components/Player"
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
--- react-player/src/components/Song.js
+++ react-player/src/components/Song.js
@@ -3,8 +3,9 @@ import React from "react";
 const Song = () => {
     return (
         <div className="song-container">
-            <h1>Song</h1>
-
+            <h1>Picture</h1>
+            <h1>Song Name</h1>
+            <h1>Artist</h1>
         </div>
     )
 }
