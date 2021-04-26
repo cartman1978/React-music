1d987ed049a716b2303ed80db116c722b56e5444
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-26 18:10:28 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 // Import Styles
 import "./styles/app.scss";
 // Adding Components
 import Song from "./components/Song";
 import Player from "./components/Player"
 import Library from "./components/Library"
+
 // Import util
 import data from "./util"
 
 function App() {
+  // Ref
+  const audioRef = useRef(null);
   //State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0
+  });
+
+  const timeupdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  }
+
   return (
     <div className="App">
       <Song currentSong={currentSong} />
       <Player
+        audioRef={audioRef}
         setIsPlaying={setIsPlaying}
         isPlaying={isPlaying}
-        currentSong={currentSong} />
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+      />
+
       <Library songs={songs} />
+      <audio
+        onTimeUpdate={timeupdateHandler}
+        onLoadedMetadata={timeupdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}></audio>
     </div>
   );
 }
