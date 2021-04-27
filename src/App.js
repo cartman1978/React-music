c6df620db90aa75238df9cb4ff7d6e9ebc0e15da
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-27 17:15:21 +0100

    styled percentage bar

--- src/App.js
+++ src/App.js
@@ -19,13 +19,18 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
-    duration: 0
+    duration: 0,
+    animationPercentage: 0
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   const timeupdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    // Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({ ...songInfo, currentTime: current, duration, animationPercentage: animation, });
   }
 
   return (
