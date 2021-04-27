ed13013434dcc239de681987ba1a2fd26a168d8d
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-27 11:04:36 +0100

    added active song functionality

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs }) => {
     const songSelectHandler = () => {
         const selectedSong = songs.filter((state) => state.id === id);
         setCurrentSong(selectedSong[0]);
+        //Add Active State
+        const newSongs = songs.map((song) => {
+            if (song.id === id) {
+                return {
+                    ...song,
+                    active: true,
+                }
+            } else {
+                return {
+                    ...song,
+                    active: false,
+                }
+            }
+        });
+        setSongs(newSongs)
         audioRef.current.play();
         // Check if song is playing
         if (isPlaying) {
@@ -16,8 +31,9 @@ const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) =
         }
     };
 
+
     return (
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
             <img src={song.cover} alt={song.name}></img>
             <div className="song-description">
                 <h3>{song.name}</h3>
